09de45296739c2cdd7aaf3d53d720931318195f6
jaca3202 <arthur.jacquemin@usherbrooke.ca>
2018-11-28 13:15:14 -0500

    test moussa

--- app.js
+++ app.js
@@ -13,25 +13,25 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-// // CORS middleware
-// app.use((req, res, next) => {
-//   // Website you wish to allow to connect
-//   res.setHeader('Access-Control-Allow-Origin', '*')
-//   // Request methods you wish to allow
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-//   // Request headers you wish to allow
-//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
-//   // Set to true if you need the website to include cookies in the requests sent
-//   // to the API (e.g. in case you use sessions)
-//   res.setHeader('Access-Control-Allow-Credentials', true)
-//   // Pass to next layer of middleware
-//   next()
-// })
+// CORS middleware
+app.use((req, res, next) => {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  // Pass to next layer of middleware
+  next()
+})
 
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'tennis-client/build')));
 
-app.use('/', indexRouter);
+app.use(indexRouter);
 app.use('/parties', partiesRouter);
 
 // Handles any requests that don't match the ones above
