23f3246b48da92ec8272518b07c9eea21b4dfb03
jaca3202 <arthur.jacquemin@usherbrooke.ca>
2018-11-28 18:37:13 -0500

    Longer matchs

--- src/generateur.js
+++ src/generateur.js
@@ -15,10 +15,10 @@ const modificateurVitesse = Math.max(process.argv[2], 1);
 
 const listePartie = [];
 
-listePartie.push(new Partie('0', new Joueur('Albert', 'Ramos', 28, 56, 'Espagne'), new Joueur('Milos', 'Raonic', 28, 16, 'Canada'), '1', 'Hale', '12h30', 0));
-listePartie.push(new Partie('1', new Joueur('Andy', 'Murray', 28, 132, 'Angleterre'), new Joueur('Andy', 'Roddick', 36, 1000, 'États-Unis'), '2', 'Hale', '13h00', 30));
-listePartie.push(new Partie('2', new Joueur('Novak', 'Djokovic', 31, 3, 'Serbie'), new Joueur('Jo-Wilfried', 'Tsonga', 33, 30, 'France'), '4', 'Hale', '14h15', 40));
-listePartie.push(new Partie('3', new Joueur('Roger', 'Federer', 37, 2, 'Suisse'), new Joueur('Rafael', 'Nadal', 32, 1, 'Espagne'), '3', 'Hale', '18h00', 70));
+listePartie.push(new Partie('0', new Joueur('Albert', 'Ramos', 28, 56, 'Espagne'), new Joueur('Milos', 'Raonic', 28, 16, 'Canada'), '1', 'Hale', '12h30', 200));
+listePartie.push(new Partie('1', new Joueur('Andy', 'Murray', 28, 132, 'Angleterre'), new Joueur('Andy', 'Roddick', 36, 1000, 'États-Unis'), '2', 'Hale', '13h00', 500));
+listePartie.push(new Partie('2', new Joueur('Novak', 'Djokovic', 31, 3, 'Serbie'), new Joueur('Jo-Wilfried', 'Tsonga', 33, 30, 'France'), '4', 'Hale', '14h15', 2000));
+listePartie.push(new Partie('3', new Joueur('Roger', 'Federer', 37, 2, 'Suisse'), new Joueur('Rafael', 'Nadal', 32, 1, 'Espagne'), '3', 'Hale', '18h00', 5000));
 
 const demarrer = function () {
   let tick = 0;
