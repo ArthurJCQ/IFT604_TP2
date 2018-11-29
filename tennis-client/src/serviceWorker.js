743f0190be07aeb0356dbb793ffe0cdd37b32bed
jaca3202 <arthur.jacquemin@usherbrooke.ca>
2018-11-29 14:38:24 -0500

    custom SW

--- tennis-client/src/serviceWorker.js
+++ tennis-client/src/serviceWorker.js
@@ -126,18 +126,6 @@ function checkValidServiceWorker(swUrl, config) {
     });
 }
 
-window.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.open('tennis-dynamic').then(function(cache) {
-      return fetch(event.request).then(function(response) {
-        cache.put(event.request, response.clone());
-        return response;
-      });
-    })
-  );
-});
-
-
 export function unregister() {
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.ready.then(registration => {
