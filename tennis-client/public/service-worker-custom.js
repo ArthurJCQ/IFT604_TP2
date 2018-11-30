389e361e8cb1cf21909cd7e1c35c74d6f0493954
jaca3202 <arthur.jacquemin@usherbrooke.ca>
2018-11-29 22:45:46 -0500

    Test load sw

--- tennis-client/public/service-worker-custom.js
+++ tennis-client/public/service-worker-custom.js
@@ -1,12 +1,12 @@
 console.log('My custom service worker');
 
-// self.addEventListener('fetch', function(event) {
-//   event.respondWith(
-//     caches.open('tennis-dynamic').then(function(cache) {
-//       return fetch(event.request).then(function(response) {
-//         cache.put(event.request, response.clone());
-//         return response;
-//       }).catch(err => console.log(err));
-//     })
-//   );
-// });
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.open('tennis-dynamic').then(function(cache) {
+      return fetch(event.request).then(function(response) {
+        cache.put(event.request, response.clone());
+        return response;
+      });
+    })
+  );
+});
