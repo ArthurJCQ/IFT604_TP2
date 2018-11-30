43ead4c8041dac6f5ba604e283f51b8d89def15a
jaca3202 <arthur.jacquemin@usherbrooke.ca>
2018-11-30 01:59:10 -0500

    Try another sw script

--- tennis-client/public/service-worker-custom.js
+++ tennis-client/public/service-worker-custom.js
@@ -11,10 +11,32 @@ console.log('My custom service worker');
 //   );
 // });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     caches.match(event.request).then(function(response) {
+//       return response || fetch(event.request);
+//     })
+//   );
+// });
+
+
+// The fetch handler serves responses for same-origin resources from a cache.
+// If no response is found, it populates the runtime cache with the response
+// from the network before returning it to the page.
+self.addEventListener('fetch', event => {
+    event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        return caches.open('tennis-dynamic').then(cache => {
+          return fetch(event.request).then(response => {
+            // Put a copy of the response in the runtime cache.
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+      })
+    );
 });
