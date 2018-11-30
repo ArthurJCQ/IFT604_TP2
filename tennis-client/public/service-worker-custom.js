f83c1f2b96e514462c1cbf58fc8ae5bceb2504d6
jaca3202 <arthur.jacquemin@usherbrooke.ca>
2018-11-30 11:04:13 -0500

    Try another Fecthing

--- tennis-client/public/service-worker-custom.js
+++ tennis-client/public/service-worker-custom.js
@@ -23,24 +23,43 @@ console.log('My custom service worker');
 // The fetch handler serves responses for same-origin resources from a cache.
 // If no response is found, it populates the runtime cache with the response
 // from the network before returning it to the page.
+// self.addEventListener('fetch', event => {
+//   // Skip cross-origin requests, like those for Google Analytics.
+//   //if (event.request.url.startsWith(self.location.origin)) {
+//     event.respondWith(
+//       caches.match(event.request).then(cachedResponse => {
+//         if (cachedResponse) {
+//           return cachedResponse;
+//         }
+//
+//         return caches.open('tennis-dynamic').then(cache => {
+//           return fetch(event.request).then(response => {
+//             // Put a copy of the response in the runtime cache.
+//             return cache.put(event.request, response.clone()).then(() => {
+//               return response;
+//             });
+//           });
+//         });
+//       })
+//     );
+//   //}
+// });
+
+
 self.addEventListener('fetch', event => {
-  // Skip cross-origin requests, like those for Google Analytics.
-  //if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
+  event.respondWith(
+    caches.open('tennis-dynamic').then(cache => {
+      return fetch(event.request).then(response => {
+        return cache.put(event.request, response.clone()).then(() => {
+          return response;
+        })
+      })
+    }).catch(() => {
       caches.match(event.request).then(cachedResponse => {
         if (cachedResponse) {
           return cachedResponse;
         }
-
-        return caches.open('tennis-dynamic').then(cache => {
-          return fetch(event.request).then(response => {
-            // Put a copy of the response in the runtime cache.
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
       })
-    );
-  //}
-});
+    })
+  )
+})
